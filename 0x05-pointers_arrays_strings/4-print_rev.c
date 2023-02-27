1d60a27f58dfbaa3495af2381e79df968d14bbdd
Tboy54321 <akinniranoluwatosin22@gmail.com>
2023-02-27 10:35:16 -0800

    changes

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*print_rev - function to print the sentence in  reverse
+*@s: The character to print
+* Return: nothing.
+*/
+
+void print_rev(char *s)
+{
+	int i = 0, l;
+
+	while (s[i] != '\0')
+		i++;
+
+	for (l = i - 1; l >= 0; l--)
+	{
+		_putchar(*(s + l));
+	}
+	_putchar('\n');
+}
---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+*puts2 - print a string.
+*@str: The character to print.
+* Return: nothing.
+*/
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '\0')
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
