7bbf0a4e273616a2314bb4208d780179f6fbf2d9
Tboy54321 <akinniranoluwatosin22@gmail.com>
2023-02-27 10:14:14 -0800

    changes

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*swap_int-swaps the values of two intengers a and b
+*@a: The character to swap
+*@b: The second character to swap
+* Return: nothing
+*/
+
+void swap_int(int *a, int *b)
+{
+	int s;
+
+	s = *a;
+	*a = *b;
+	*b = s;
+}
